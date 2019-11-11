d33b78ef78fb5019088a5bb519d4edb9dff13885
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2019-11-11 02:00:39 +0000

    update destaque victor 23:00H

--- src/app/services/destaque.service.ts
+++ src/app/services/destaque.service.ts
@@ -1,38 +1,25 @@
+import { Destaque } from './../models/destaque.model';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-//import { Jogos } from "../services/interfaces/jogo.interface";
-//import { Jogo } from "../models/jogos.model";
-
-export interface Jogo {
-    name: string;
-    idJogo:     number;
-    tituloJogo: string;
-    descJogo:   string;
-    tagsJogo:   string;
-    imagem1:    string;
-    imagem2:    string;
-    imagem3:    string;
-    imagem4:    string;
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class DestaqueService {
   constructor(public http: HttpClient) {
-    console.log("Service Ativado");
+    console.log("Service Destaque Ativado");
   }
 
   getJogos() {
-    return this.http.get<Jogo[]>(
+    return this.http.get<Destaque[]>(
       "https://sleepy-river-60466.herokuapp.com/jogos/d"
     );
   }
 
   getJogoID(idJogo) {
-    return this.http.get<Jogo>("https://sleepy-river-60466.herokuapp.com/jogos/p?id="+idJogo);
+    return this.http.get<Destaque[]>("https://sleepy-river-60466.herokuapp.com/jogos/p?id="+idJogo);
   }
 
 }
