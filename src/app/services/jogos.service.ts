7ffb426f0ac144482a408342175a74a93392b928
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2019-11-11 13:27:35 +0000

    updates jogos.service

--- src/app/services/jogos.service.ts
+++ src/app/services/jogos.service.ts
@@ -2,31 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-//import { Jogos } from "../services/interfaces/jogo.interface";
-//import { Jogo } from "../models/jogos.model";
-
-export interface Jogo {
-    name: string;
-    idJogo:     number;
-    tituloJogo: string;
-    descJogo:   string;
-    tagsJogo:   string;
-    imagem1:    string;
-    imagem2:    string;
-    imagem3:    string;
-    imagem4:    string;
-}
+import { Jogo } from "../models/jogos.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class JogosService {
-  url = "https://sleepy-river-60466.herokuapp.com/jogos/t";
-  url1 = "https://sleepy-river-60466.herokuapp.com/jogos/p?id=1";
-  url2 = "https://sleepy-river-60466.herokuapp.com/jogos"
-
   constructor(public http: HttpClient) {
-    console.log("Service Ativado");
+    console.log("Service Jogo Ativado");
   }
 
   getJogos() {
@@ -36,6 +19,6 @@ export class JogosService {
   }
 
   getJogoID(idJogo) {
-    return this.http.get<Jogo>("https://sleepy-river-60466.herokuapp.com/jogos/p?id="+idJogo);
+    return this.http.get<Jogo[]>("https://sleepy-river-60466.herokuapp.com/jogos/p?id="+idJogo);
   }
 }
