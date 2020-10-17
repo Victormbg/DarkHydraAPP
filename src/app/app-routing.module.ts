4c1f936ed6df5b5e82efc7deb200dcf9fdc3afc7
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-10-17 14:30:59 +0000

    mudanças de formatacao e aspas simples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -81,9 +81,18 @@ const routes: Routes = [
     path: "comunidade",
     loadChildren: "./pages/comunidade/comunidade.module#ComunidadePageModule"
   },
-  { path: 'regras', loadChildren: './pages/regras/regras.module#RegrasPageModule' },
-  { path: 'seguindo', loadChildren: './pages/seguindo/seguindo.module#SeguindoPageModule' },
-  { path: 'seguidores', loadChildren: './pages/seguidores/seguidores.module#SeguidoresPageModule' }
+  {
+    path: "regras",
+    loadChildren: "./pages/regras/regras.module#RegrasPageModule"
+  },
+  {
+    path: "seguindo",
+    loadChildren: "./pages/seguindo/seguindo.module#SeguindoPageModule"
+  },
+  {
+    path: 'seguidores',
+    loadChildren: './pages/seguidores/seguidores.module#SeguidoresPageModule'
+  }
 ];
 
 @NgModule({
