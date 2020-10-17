245ee74b43bde8804728363c373901e3690ec46c
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-10-17 14:35:33 +0000

    adicionando comandos Firebase

--- src/app/services/jogos.service.ts
+++ src/app/services/jogos.service.ts
@@ -1,33 +1,60 @@
-import { Amigos } from './../models/amigos';
 import { Injectable } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject
+} from '@angular/fire/database';
+
 import { Jogo } from '../models/jogos';
+import { Amigos } from './../models/amigos';
+
 import { Comentario } from './../models/comentarios';
-import * as firebase from 'firebase';
-const firebaseConfig = {
-    apiKey: 'AIzaSyCWgMOkOknFuNi5olqGBnM3hgnfg57WUbA',
-    authDomain: 'darkhydra-1572877735000.firebaseapp.com',
-    databaseURL: 'https://darkhydra-1572877735000.firebaseio.com',
-    projectId: 'darkhydra-1572877735000',
-    storageBucket: 'darkhydra-1572877735000.appspot.com',
-    messagingSenderId: '725846488547',
-    appId: '1:725846488547:web:7c9eea2eb934e0bdd0dcd6'
-};
-    
 
-var database = firebase.database();
-
-var banco = database.ref('Jogos/')
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class JogosService {
-  constructor(public http: HttpClient) {
-    console.log("Service Jogo Ativado");
-    
+  JogoListRef: AngularFireList<any>;
+  JogoRef: AngularFireObject<any>;
+  constructor(public http: HttpClient, private db: AngularFireDatabase) {
+    console.log('Service Jogo Ativado');
   }
+
+  getJogos() {
+    this.JogoListRef = this.db.list('/Jogos');
+    console.log(this.JogoListRef);
+    return this.JogoListRef;
+  }
+
+  getJogoID(idJogo) {
+    this.JogoListRef = this.db.list('/Jogos' + idJogo);
+    console.log(this.JogoListRef);
+    return this.JogoListRef;
+  }
+
+    getComentarios(idJogo) {
+    this.JogoListRef = this.db.list('/Jogos' + idJogo);
+    console.log(this.JogoListRef);
+    return this.JogoListRef;
+  }
+
+  getBiblioteca() {
+    this.JogoListRef = this.db.list('/Jogos');
+    console.log(this.JogoListRef);
+    return this.JogoListRef;
+  }
+
+  getJogoD(idSeguido) {
+    this.JogoListRef = this.db.list('/Jogos' + idSeguido);
+    console.log(this.JogoListRef);
+    return this.JogoListRef;
+  }
+}
 /*
   getJogos() {
     return this.http.get<Jogo[]>(
@@ -59,30 +86,3 @@ export class JogosService {
     );
   }
 */
-
-
-
-
-getJogos() {
-    return 
-  }
-
-
-
-
-
-
-
-
-module.exports.LerNomesJogos = function LerNomesJogos() {
-    banco.once("value", function (snapshot) {
-        var Nome = [];
-        snapshot.forEach(function (childSnapshot) {
-            var getNome = childSnapshot.val().Nome_Do_Jogo;
-            Nome.push(getNome);
-            return this.childSnapshot;
-        });
-        return Nome;
-    });
-};
-}
