745a2f40fe87b3b93fbf9e5577a84aac8b28230b
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-10-17 15:01:53 +0000

    melhorando a funcao getJogos

--- src/app/services/jogos.service.ts
+++ src/app/services/jogos.service.ts
@@ -1,60 +1,90 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 import {
   AngularFireDatabase,
   AngularFireList,
   AngularFireObject
-} from '@angular/fire/database';
+} from "@angular/fire/database";
 
-import { Jogo } from '../models/jogos';
-import { Amigos } from './../models/amigos';
+import { Jogo } from "../models/jogos";
+import { Amigos } from "./../models/amigos";
 
-import { Comentario } from './../models/comentarios';
+import { Comentario } from "./../models/comentarios";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class JogosService {
-  JogoListRef: AngularFireList<any>;
-  JogoRef: AngularFireObject<any>;
+  jogos: AngularFireList<any>;
+  //JogoListRef: AngularFireList<any>;
+  //JogoRef: AngularFireObject<any>;
   constructor(public http: HttpClient, private db: AngularFireDatabase) {
-    console.log('Service Jogo Ativado');
+    console.log("Service Jogo Ativado");
   }
 
   getJogos() {
-    this.JogoListRef = this.db.list('/Jogos');
-    console.log(this.JogoListRef);
-    return this.JogoListRef;
+    this.jogos = this.db.list("/Jogos");
+    this.jogos.snapshotChanges(["child_added"]).subscribe(actions => {
+      actions.forEach(action => {
+        let tituloJogo = action.payload.val().Titulo_Do_Jogo;
+        console.log(tituloJogo);
+      });
+      return this.jogos;
+    });
   }
 
   getJogoID(idJogo) {
-    this.JogoListRef = this.db.list('/Jogos' + idJogo);
-    console.log(this.JogoListRef);
-    return this.JogoListRef;
+    console.log(this.db);
+    this.jogos = this.db.list("/Jogos");
+    this.jogos.snapshotChanges(["child_added"]).subscribe(actions => {
+      actions.forEach(action => {
+        console.log(action.key);
+        console.log(action.payload.val());
+      });
+      return this.jogos;
+    });
   }
 
-    getComentarios(idJogo) {
-    this.JogoListRef = this.db.list('/Jogos' + idJogo);
-    console.log(this.JogoListRef);
-    return this.JogoListRef;
+  getComentarios(idJogo) {
+    console.log(this.db);
+    this.jogos = this.db.list("/Jogos");
+    this.jogos.snapshotChanges(["child_added"]).subscribe(actions => {
+      actions.forEach(action => {
+        console.log(action.key);
+        console.log(action.payload.val());
+      });
+      return this.jogos;
+    });
   }
 
   getBiblioteca() {
-    this.JogoListRef = this.db.list('/Jogos');
-    console.log(this.JogoListRef);
-    return this.JogoListRef;
+    console.log(this.db);
+    this.jogos = this.db.list("/Jogos");
+    this.jogos.snapshotChanges(["child_added"]).subscribe(actions => {
+      actions.forEach(action => {
+        console.log(action.key);
+        console.log(action.payload.val());
+      });
+      return this.jogos;
+    });
   }
 
   getJogoD(idSeguido) {
-    this.JogoListRef = this.db.list('/Jogos' + idSeguido);
-    console.log(this.JogoListRef);
-    return this.JogoListRef;
+    this.jogos = this.db.list("/Jogos");
+    this.jogos.snapshotChanges(["child_added"]).subscribe(actions => {
+      actions.forEach(action => {
+        console.log(action.key);
+        console.log(action.payload.val());
+      });
+      return this.jogos;
+    });
   }
 }
+
 /*
   getJogos() {
     return this.http.get<Jogo[]>(
