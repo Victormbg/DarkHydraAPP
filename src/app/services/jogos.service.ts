d01b4345e6e49d315c1910d91c3b6e399d0319a5
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-10-17 14:15:23 +0000

    mudanças para pegar os jogos no firebase

--- src/app/services/jogos.service.ts
+++ src/app/services/jogos.service.ts
@@ -1,19 +1,34 @@
 import { Amigos } from './../models/amigos';
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
-import { Jogo } from "../models/jogos";
-import { Comentario } from "./../models/comentarios";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Jogo } from '../models/jogos';
+import { Comentario } from './../models/comentarios';
+import * as firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: 'AIzaSyCWgMOkOknFuNi5olqGBnM3hgnfg57WUbA',
+    authDomain: 'darkhydra-1572877735000.firebaseapp.com',
+    databaseURL: 'https://darkhydra-1572877735000.firebaseio.com',
+    projectId: 'darkhydra-1572877735000',
+    storageBucket: 'darkhydra-1572877735000.appspot.com',
+    messagingSenderId: '725846488547',
+    appId: '1:725846488547:web:7c9eea2eb934e0bdd0dcd6'
+};
+    
 
+var database = firebase.database();
+
+var banco = database.ref('Jogos/')
 @Injectable({
   providedIn: "root"
 })
 export class JogosService {
   constructor(public http: HttpClient) {
     console.log("Service Jogo Ativado");
+    
   }
-
+/*
   getJogos() {
     return this.http.get<Jogo[]>(
       "https://sleepy-river-60466.herokuapp.com/jogos/t"
@@ -43,5 +58,31 @@ export class JogosService {
       "https://sleepy-river-60466.herokuapp.com/jogos/e?u=" + idSeguido
     );
   }
+*/
 
+
+
+
+getJogos() {
+    return 
+  }
+
+
+
+
+
+
+
+
+module.exports.LerNomesJogos = function LerNomesJogos() {
+    banco.once("value", function (snapshot) {
+        var Nome = [];
+        snapshot.forEach(function (childSnapshot) {
+            var getNome = childSnapshot.val().Nome_Do_Jogo;
+            Nome.push(getNome);
+            return this.childSnapshot;
+        });
+        return Nome;
+    });
+};
 }
